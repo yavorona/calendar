1e72c36067c30e362386d093ffe419aaa079de67
Bryce Balbon <bbalbon@gmail.com>
2020-01-28 22:12:32 -0800

    Complete first Enzyme test

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from "./enzyme.js";
+import CheckIn from '../client/src/components/checkin.jsx';
+
+it('should render checkIn button with correct text', () => {
+    const wrapper = shallow(<CheckIn />)
+    const button = wrapper.find('button');
+    const result = button.text();
+
+    expect(result).toBe('This is the checkin mod');
+});
